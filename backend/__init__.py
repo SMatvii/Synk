a9449ea23ed5e05018a263d4c7b7e7d4d1999cc7
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-01-06 19:45:13 +0100

    ...

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,5 @@
 from .db import migrate
-from .routes import app, post_router, user_router, comment_router
+from .routes import app, post_router
 
 
 app.include_router(post_router)
-app.include_router(user_router)
-app.include_router(comment_router)
