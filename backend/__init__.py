be74f122c26d0fa7eb837fcd65603ef00cb7f576
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-01-09 18:47:05 +0100

    ...

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from .db import migrate
-from .routes import app, post_router, user_router, comment_router
-
-
-app.include_router(post_router)
-app.include_router(user_router)
-app.include_router(comment_router)
--- backend/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-from .user import user_router
