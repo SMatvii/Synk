7b566adc145a1cb2f5dabcf1701c37b44a3297ae
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-01-13 20:03:53 +0100

    unittest for comment post endpoint added

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 
-<<<<<<< HEAD
-from .routes import comment_router, auth_router, post_router, user_router, upload_router
+
+from .routes import comment_router, auth_router, post_router, user_router, upload_router,subscribe_router
 from .db import migrate, Config, User, Post
-=======
-from .routes import comment_router, auth_router, post_router, user_router, upload_router, subscribe_router
-from .db import migrate
->>>>>>> 15ab731bfd8102f3e2c8f4d53bdda954dc43f064
+
 
 app = FastAPI()
 
