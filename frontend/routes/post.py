a302a3c4dcb0db46c99b664c3040b9d67636c779
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-01-17 16:07:14 +0100

    check if comments

--- frontend/routes/post.py
+++ frontend/routes/post.py
@@ -9,11 +9,19 @@ def see_one_post(id):
     if post:
         user_id = post.json().get("user_id")
         user = get(f"{BACKEND_URL}/users/{user_id}")
-        comments = get(f"{BACKEND_URL}/comments/{id}") 
-        return render_template(
-            "post.html", 
-            post=post.json(), 
-            user=user.json(),
-            comments=comments.json()
+        comments = get(f"{BACKEND_URL}/comments/{id}")
+        if comments.status_code == 200: 
+            return render_template(
+                "post.html", 
+                post=post.json(), 
+                user=user.json(),
+                comments=comments.json()
+                )
+        else:
+            return render_template(
+                "post.html", 
+                post=post.json(), 
+                user=user.json(),
             )
+
 
