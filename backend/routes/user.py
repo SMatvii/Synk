87b4e957950638ff0be58e94f1b6c951ab20a09e
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-01-21 18:53:51 +0100

    return current user

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -64,3 +64,11 @@ def delete_user(
 
     session.commit()
     return {"detail": f"User with id {current_user.id} deleted successfully"}
+
+
+
+@user_router.get("/current_user")
+def get_currentuser(current_user: Annotated[User, Depends(get_current_user)],):
+    return current_user
+
+ 
